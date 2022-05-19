6849c0fb1105ca9d30115078ecadeac346fe945f
user.name <mohannatrayan.1997@gmail.com>
2022-05-19 09:52:58 +0530

    swap

---
+++ swap.java
@@ -0,0 +1,15 @@
+public class swap {
+
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 16;
+        int c  = a;
+
+        a=b;
+        b=c;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+    
+}
